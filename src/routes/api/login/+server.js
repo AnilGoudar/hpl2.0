e20c8cb8fb8cceeb89eb01454ab81b4f8b6a6c8c
anil-tuggaladoni <anilgoudar369@gmail.com>
2025-12-08 19:33:58 +0530

    revert cookie

--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -26,15 +26,15 @@ export async function POST({ request, cookies }) {
 	cookies.set('hpl_access_token', session.access_token, {
 		path: '/',
 		httpOnly: true,
-		sameSite: 'none',
-		secure: true,
+		sameSite: 'lax',
+		secure: process.env.NODE_ENV === 'production',
 		maxAge: 60 * 60 * 24 * 10
 	});
 	cookies.set('hpl_refresh_token', session.refresh_token, {
 		path: '/',
 		httpOnly: true,
-		secure: true,
-		sameSite: 'none',
+		secure: process.env.NODE_ENV === 'production',
+		sameSite: 'lax',
 		maxAge: 60 * 60 * 24 * 30
 	});
 	return json(
